b1938a4d21f0c536d688b435f28ba40d938d5604
idhun90 <idhun90@naver.com>
2022-07-16 23:24:50 +0900

    회원가입 완료 로그인 화면 구성

---
+++ Netflix Project SignUp/SecondViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondViewController.swift
+//  Netflix Project SignUp
+//
+//  Created by 이도헌 on 2022/07/16.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var OkButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        designOkButton()
+        resultLabel.text = "ㅇㅇ님 회원가입을 축하드립니다."
+        resultLabel.textColor = .white
+        view.backgroundColor = .black
+    }
+    
+    func designOkButton() {
+        OkButton.layer.masksToBounds = true
+        OkButton.layer.cornerRadius = 5
+        OkButton.setTitle("확인", for: .normal)
+        OkButton.setTitleColor(.black, for: .normal)
+        OkButton.setTitleColor(.gray, for: .highlighted)
+        OkButton.backgroundColor = .white
+    }
+}
